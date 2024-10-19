097a1e58a9f3cf44d6f0d5c0a3fe0790da472ec8
Elizaveta Pokhlebkina <foxxybeat@gmail.com>
2024-10-19 23:27:10 +1100

    123

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '158.160.88.41', 'localhost', 'ftaski.ddns.net']
+ALLOWED_HOSTS = ['ftaski.ddns.net', '127.0.0.1', '158.160.88.41', 'localhost']
 
 
 # Application definition
